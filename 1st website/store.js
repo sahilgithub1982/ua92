590c9b5839a55c9ea0110a2187bc913fe8040d91
sahilgithub1982 <125290703+sahilgithub1982@users.noreply.github.com>
2023-02-17 02:05:27 +0000

    Add files via upload

---
+++ 1st website/store.js
@@ -0,0 +1,11 @@
+var removeCartItemButtons = document.getElementsByClassName('Buy Now')
+console.log(removeCartItemButtons)
+for (var i =0; i < removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click',function(event){
+        var buttonclicked = event.target
+        
+
+    })
+    
+}
